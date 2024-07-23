73194f2cd53881c0233faa2c9002eb3cfe8c51f8
Muskantrivedi6 <133230817+Muskantrivedi6@users.noreply.github.com>
2024-07-23 18:42:59 +0530

    Day-1 files

---
+++ Day_1-Assignment.js
@@ -0,0 +1,17 @@
+// variable f type String and add a number
+
+let b = "4"
+let c = 6;
+
+console.log(b+c)
+
+// Use typeof operator to find the datatype of string
+let d = "hello"
+console.log(typeof(b+c))
+
+//Can you change it to hold a number later
+const obj ={
+    "herry" : 4,
+
+}
+obj ="o"
